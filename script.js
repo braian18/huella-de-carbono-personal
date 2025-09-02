904075160ce1518aa9c7cd3ebe7b6e00b8f9ed91
francor36 <frankoxdsave@gmail.com>
2025-09-01 23:45:43 -0300

    cambios en el nav modo osc claro

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {/*Aca va todo el codigo script para que se cargue bien el DOM */});
+document.addEventListener('DOMContentLoaded', () => {
+    /*Aca va todo el codigo script para que se cargue bien el DOM */
+    const botonD = document.getElementById("oscuro");
+    const nav = document.querySelector('nav');
+    const body = document.querySelector('body');
+
+    botonD.addEventListener("click", () => {
+        nav.classList.remove("claro");
+        body.classList.remove("natural");
+        nav.classList.add("dark");
+        body.classList.add("oscuro");
+    })
+
+});
