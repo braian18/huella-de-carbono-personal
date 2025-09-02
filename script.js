b3fb26f28d023a1a7779d7e21a1ddfce11206492
braian18 <braianvillarroel70@gmail.com>
2025-09-02 16:55:49 -0300

    diseño responsive y finalizacion de funcionalidad del boton modo oscur y claro

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*Aca va todo el codigo script para que se cargue bien el DOM */
     const botonD = document.getElementById("oscuro");
     const nav = document.querySelector('nav');
     const body = document.querySelector('body');
 
     botonD.addEventListener("click", () => {
-        nav.classList.remove("claro");
-        body.classList.remove("natural");
-        nav.classList.add("dark");
-        body.classList.add("oscuro");
-    })
-
+        if(body.classList.contains("oscuro")){
+            // Si ya está oscuro, volvemos a claro
+            nav.classList.remove("dark");
+            body.classList.remove("oscuro");
+            nav.classList.add("claro");
+            body.classList.add("natural");
+        } else {
+            // Si está claro, vamos a oscuro
+            nav.classList.remove("claro");
+            body.classList.remove("natural");
+            nav.classList.add("dark");
+            body.classList.add("oscuro");
+        }
+    });
 });
